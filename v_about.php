3a25903d6d8b44b33aedac3077acc53d52678f5c
junapurba10 <90760357+junapurba10@users.noreply.github.com>
2021-10-02 18:56:33 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+ 	<h4>Nama</h4>
+ 	<ul type="disc">
+ 		<li>Nama Depan : Junaidi</li>
+ 		<li>Nama Belakang : Purba</li>
+ 	</ul>
+ <br>
+ 	<h4>Alamat</h4>
+ 	<ul type="none">
+ 		<li> Jln. Bumi Persegi No.666</li>
+ 	</ul>
+ 
+ 	<h4>Tempat Lahir</h4>
+ 	<ul type="none">
+ 		<li>Bumi Sebelah Tenggara</li>
+ 	</ul>
+ 	<h4>Sebuah Teori Konspirasi</h4>
+ 	<ul type="square">
+ 		<li>Apakah Bumi itu Bulat ??</li>
+ 		<li>Atau Semesta ini yang Persegi ??</li>
+ 	</ul>
+</section>
